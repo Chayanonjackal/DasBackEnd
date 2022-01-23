81e83c83bd18764405dab75581296ffd3cf7c41a
Chayanonjackal <aom_jackal@hotmail.com>
2022-01-23 15:09:31 +0700

    update bcypt

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -8,23 +8,6 @@ const auth = require('../middleWare/auth');
 const bcrypt = require('bcryptjs');
 require('dotenv').config()
 
-//register user v1
-// user.post('/post-user', (req, res) => {
-//     const userData = {
-//         user_name: req.body.user_name,
-//         password: req.body.password,
-//         first_name: req.body.first_name,
-//         last_name: req.body.last_name,
-//         role: req.body.role
-//     }
-//     userModel.create(userData)
-//         .then((user) => {
-//             res.status(200).json("Create Success");
-//         })
-//         .catch((error) => {
-//             res.send(error);
-//         })
-// })
 
 //register api uer v2
 user.post('/register', (req, res) => {
@@ -52,16 +35,25 @@ user.post('/register', (req, res) => {
         }).then((value) => {
             if (value === null) {
                 //if no data found create new row
-                userModel.create(userData).then(value => {
-                    res.status(201).json({
-                        message: "Account has create sucessfully",
+                //encypt here
+                const saltRounds = 10;
+                
+                bcrypt.hash(userData.password, saltRounds, function(err, hash) {
+                    // Store hash in your password DB.
+                    userData.password = hash
+                    userModel.create(userData).then(value => {
+                        res.status(201).json({
+                            message: "Account has create sucessfully",
+                            status: res.statusCode
+                        })
+    
+                    }).catch(error => res.status(404).json({
+                        message: "Some thing went wrong",
                         status: res.statusCode
-                    })
+                    }))
 
-                }).catch(error => res.status(404).json({
-                    message: "Some thing went wrong",
-                    status: res.statusCode
-                }))
+                });
+
             } else {
                 res.status(401).json({
                     message: "username is already taken",
@@ -75,32 +67,6 @@ user.post('/register', (req, res) => {
 
 })
 
-//get token user login api v1
-// user.post('/user-login', async (req, res) => {
-//     try {
-
-//         const user = await userModel.findOne({
-//             where:
-//             {
-//                 user_name: req.body.user_name,
-//                 password: req.body.password
-//             }
-//         })
-//         if (user == null) {
-//             //if user null
-//             return res.status(200).json('Invalid user')
-//         } else {
-//             //if not null
-//             var token = {
-//                 token: jwt.sign(user.dataValues, '1234')
-//             };
-//             return res.status(200).json(token)
-//         }
-//     } catch (error) {
-//         return res.json(error)
-//     }
-
-// })
 
 //login api
 user.post('/login', (req, res) => {
@@ -131,34 +97,38 @@ user.post('/login', (req, res) => {
             } else {
                 //if username right but password is correct or not
                 const dbUserPassword = value.getDataValue('password') //aom1234 from base
-                // console.log(userData.password);
-                // console.log(dbUserPassword);
                 // bcrypt.compare(userData.password, dbUserPassword, function (err, result) { })
-                // console.log(userData.password == dbUserPassword);
-                if (userData.password == dbUserPassword) {
-                    //if password is correct send json web token
-                    const userDetail = {
-                        name: value.getDataValue('user_name'),
-                        id: value.getDataValue('user_id'),
-                        fname: value.getDataValue('first_name'),
-                        lname: value.getDataValue('last_name'),
-                        role: value.getDataValue('role')
+                bcrypt.compare(userData.password, dbUserPassword, function(err, result) {
+                    // result == true
+                    console.log("PassWord : "+result);
+
+                    if (result) {
+                        //if password is correct send json web token
+                        const userDetail = {
+                            name: value.getDataValue('user_name'),
+                            id: value.getDataValue('user_id'),
+                            fname: value.getDataValue('first_name'),
+                            lname: value.getDataValue('last_name'),
+                            role: value.getDataValue('role')
+                        }
+                        // time out set {expiresIn:"60s"}
+                        const token = jwt.sign(userDetail, process.env.secret_key, { expiresIn: "1800s" })  //'1234'
+                        res.status(200).json({
+                            message: "Logged is successfully",
+                            status: res.statusCode,
+                            token
+                        })
+                    } else {
+                        //if password is not match
+                        res.status(401).json({
+                            message: "Invalid Crendential Given"
+                            , status: res.statusCode
+                            , token: ''
+                        })
                     }
-                    // time out set {expiresIn:"60s"}
-                    const token = jwt.sign(userDetail, process.env.secret_key, { expiresIn: "1800s" })  //'1234'
-                    res.status(200).json({
-                        message: "Logged is successfully",
-                        status: res.statusCode,
-                        token
-                    })
-                } else {
-                    //if password is not match
-                    res.status(401).json({
-                        message: "Invalid Crendential Given"
-                        , status: res.statusCode
-                        , token: ''
-                    })
-                }
+                });
+                
+                
 
             }
 
@@ -207,17 +177,7 @@ user.get('/get-all-user', auth, (req, res) => {
     userModel.findAll()
         .then((user) => {
             res.status(200).json(user)   // need to hashfunction value password   or  Encyption and Decyption
-            // res.status(200).json({
-            //     message:"get all profile success",
-            //     status:res.statusCode ,
-            //     data:{
-            //         user_id:user.getDataValue('user_id'),
-            //         user_name:user.getDataValue('user_name'),
-            //         first_name:user.getDataValue('first_name'),
-            //         last_name:user.getDataValue('last_name'),
-            //         role:user.getDataValue('role')
-            //     }
-            // })
+    
         })
         .catch((error) => {
             res.status(400).json({
@@ -330,25 +290,6 @@ user.delete('/delete-ppdata-userdata', auth, (req, res) => {
 
 })
 
-// user.post("/check-delete", auth, ((req, res) => {
-//     if (req.body.user_id !== undefined && req.body.user_id !== '') {
-//         ppModel.findAll({
-//             where: {
-//                 user_id: req.body.user_id
-//             }
-//         }).then(data => {
-//             console.log(data);
-//             if (data = []) {
-
-//             }
-//         })
-//     } else {
-//         res.status(400).json({
-//             message: " Can't Delete something wrong "
-//             , status: res.statusCode
-//         })
-//     }
-// }))
 
 user.put('/edit-user', auth, (req, res) => {
     const userData = {
@@ -371,7 +312,8 @@ user.put('/edit-user', auth, (req, res) => {
                 user_name: req.body.user_name
             }
         }).then(call => {
-            // console.log(call.dataValues);
+         
+
             if (call) {
                 if (call.dataValues.user_id == req.body.user_id) {
                     userModel.findOne({
@@ -381,14 +323,21 @@ user.put('/edit-user', auth, (req, res) => {
                     })
                         .then(data => {
                             if (data) {
-                                // console.log(data.dataValues);
-                                userModel.update(userData, { where: { user_id: req.body.user_id } })
+                                const saltRounds = 10;
+                                bcrypt.hash(userData.password, saltRounds, function(err, hash) {
+                                    userData.password = hash ;
+                                    console.log('userPassword : '+ userData.password);
+                                    userModel.update(userData, { where: { user_id: req.body.user_id } })
                                     .then(call => {
                                         res.status(200).json({
                                             message: "Update sucessful",
                                             status: res.statusCode
                                         })
                                     })
+                                });
+
+                              
+
                             } else {
                                 res.status(400).json({
                                     message: "No user Profile for update",
@@ -453,22 +402,6 @@ user.post('/profile-user', auth, (req, res) => {
 })
 
 //when create api u should use middleWare auth to check userToken when user call api
-//get data
-
-// user.post('/get-user-data', auth, async (req, res) => {
-//     try {
-//         const user = await userModel.findOne({
-//             where:
-//             {
-//                 user_id: req.body.user_id
-//             }
-//         })
-//         // var token = jwt.sign(user.dataValues,'1234')
-//         return res.status(200).json(user)
-//     } catch (error) {
-//         return res.json(error)
-//     }
-// })
 
 
 
