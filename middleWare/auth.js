e3bc1ffaf7a5a174a123df727383faae11418545
FROSSTY <aom_jackal@hotmail.com>
2022-03-17 03:55:02 +0700

    #UPDATE delete comment

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -2,35 +2,7 @@ const { verify } = require("jsonwebtoken");
 const userModel = require('../Models/User');
 const jwt = require("jsonwebtoken");
 require('dotenv').config()
-// module.exports = function auth(req,res ,next){
-//     try {
-//         const token = req.headers.token ;
-//         console.log(token);
-//         if(!token){
-//             return res.status(400).json({ error: "Unauthorized" });
-//         }
-//         verify(token,'1234' , async (err, decode) => {
-//                 if(err){
-//                     return res.status(400).json({ error: "Token Error" });  
-//                 }
 
-//                 const user = await userModel.findOne({
-//                     where: { user_id: decode.user_id },
-//                   });
-//                 if(user){
-//                     // return  res.status(400).json({ error: "User Invalid" });  
-//                     console.log(user);
-//                     return next();
-//                 }else{
-//                     return  res.status(400).json({ error: "User Invalid" });  
-//                 }
-
-//         })
-
-//     } catch (error) {
-//         return res.status(400).json("Bad request") ;
-//     }
-// }
 
 module.exports = function auth(req, res, next) {
     try {
